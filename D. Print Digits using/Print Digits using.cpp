75eef98876c22f4de5cf1f80a088a478cdbc98d3
sara2005-tech <sara23013799@gmail.com>
2026-03-01 00:07:40 +0200

    Update Print Digits using.cpp

--- D. Print Digits using/Print Digits using.cpp
+++ D. Print Digits using/Print Digits using.cpp
@@ -4,27 +4,33 @@ using namespace std;
 void printDigits(long long n)
 {
     if(n < 10)
-    cout << n << " ";
-        return;  }
+    {
+        cout << n << " ";
+        return;
+    }
 
     printDigits(n / 10);
     cout << n % 10 << " ";
 }
+
 int main()
 {
-    int n;
-    cin >> n;
-    while(n--)
+    int T;
+    cin >> T;
+
+    for(int i = 0; i < T; i++)
     {
         long long N;
         cin >> N;
 
-        if(N == 0)
+        if(N == 0) 
             cout << 0;
         else
             printDigits(N);
+
         cout << endl;
     }
 
     return 0;
 }
+
